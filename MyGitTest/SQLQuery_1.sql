74d86fbc90a428d7f50368a293b2f459d4f621f9
waynelxb <waynelxb@gmail.com>
2021-10-21 15:27:18 -0500

    stage

---
+++ MyGitTest/SQLQuery_1.sql
@@ -0,0 +1,36 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE VIEW [SalesLT].[vProductAndDescription]
+WITH SCHEMABINDING
+AS
+-- View (indexed or standard) to display products and product descriptions by language.
+SELECT
+    p.[ProductID]
+    ,p.[Name]
+    ,pm.[Name] AS [ProductModel]
+    ,pmx.[Culture]
+    ,pd.[Description]
+FROM [SalesLT].[Product] p
+    INNER JOIN [SalesLT].[ProductModel] pm
+    ON p.[ProductModelID] = pm.[ProductModelID]
+    INNER JOIN [SalesLT].[ProductModelProductDescription] pmx
+    ON pm.[ProductModelID] = pmx.[ProductModelID]
+    INNER JOIN [SalesLT].[ProductDescription] pd
+    ON pmx.[ProductDescriptionID] = pd.[ProductDescriptionID];
+GO
+SET ARITHABORT ON
+SET CONCAT_NULL_YIELDS_NULL ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+SET NUMERIC_ROUNDABORT OFF
+GO
+CREATE UNIQUE CLUSTERED INDEX [IX_vProductAndDescription] ON [SalesLT].[vProductAndDescription]
+(
+	[Culture] ASC,
+	[ProductID] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
